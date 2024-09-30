8f6f6b69f978dd2c8ad8e1a184c27c4a1c73e538
masamonoke <m.rudakov@yahoo.com>
2024-09-30 22:08:32 +0300

    Init

---
+++ build.sh
@@ -0,0 +1,10 @@
+# 1. Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# 2. Install Powerlevel10k Theme
+git clone https://github.com/romkatv/powerlevel10k $ZSH_CUSTOM/themes/powerlevel10k
+# 3. Install Auto Suggestions Plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+# 4. Install Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# 5. Set theme to (includes the quotes)
+# "powerlevel10k/powerlevel10k"
